b84fc1473ad6ef11757bee514753a6ef0403e096
Axblom Mikael <mikael.axblom@softronic.se>
2018-12-13 08:54:41 +0100

    Add project files.

---
+++ ObjectDumper/DumpObjectCommand.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Globalization;
+using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+using Microsoft.VisualStudio.PlatformUI;
+
+namespace ObjectDumper
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class DumpObjectCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("e352a0e9-68b9-427d-87f5-7db47da08b18");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DumpObjectCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private DumpObjectCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static DumpObjectCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in DumpObjectCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+            Instance = new DumpObjectCommand(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            DTE dte;
+
+            dte = (DTE)this.ServiceProvider.GetServiceAsync(typeof(DTE)).Result;
+
+            var debugger = dte.Debugger;
+            var locals = debugger.CurrentStackFrame.Locals;
+
+            var localList = new List<Expression>();
+            foreach (Expression item in locals)
+            {
+                localList.Add(item);
+            }          
+            var dialog = new ExportDialog(localList);
+            dialog.ShowDialog();
+        }
+    }
+}
---
+++ ObjectDumper/ExportDialog.xaml.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ObjectDumper
+{
+    /// <summary>
+    /// Interaction logic for WpfDialog.xaml
+    /// </summary>
+    public partial class ExportDialog : System.Windows.Window
+    {
+        private List<EnvDTE.Expression> Locals { get; set; }
+        public ExportDialog(List<EnvDTE.Expression> locals)
+        {
+            InitializeComponent();
+            Locals = locals;
+            PopulateDropDown(locals);
+        }
+
+        
+        private void PopulateDropDown(List<EnvDTE.Expression> locals)
+        {
+            try
+            {
+                Dispatcher.VerifyAccess();
+                locals.ForEach(i => LocalDropDown.Items.Add(i.Name));
+            }
+            catch (Exception ex)
+            {
+                TypeInfo.Text = $"Exception of type {ex.GetType()} occured";
+                OutPut.Text = ex.ToString();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private Stopwatch RunTimeTimer { get; } = new Stopwatch();
+
+        private void LocalDropDown_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            try
+            {
+                RunTimeTimer.Start();
+                Dispatcher.VerifyAccess();
+                var dropDown = sender as ComboBox;
+                var selectedLocal = Locals.FirstOrDefault(i => i.Name == dropDown.SelectedValue.ToString());
+
+                var json = GenerateJson(selectedLocal);
+                json = json.TrimEnd(',');
+                OutPut.Text = "{" + json + "}";
+                TypeInfo.Text = selectedLocal.Type.ToString();
+            }
+            catch (Exception ex)
+            {
+                TypeInfo.Text = $"Exception of type {ex.GetType()} occured";
+                OutPut.Text = ex.ToString();
+            }
+            RunTimeTimer.Stop();
+            RunTimeTimer.Reset();
+        }
+
+        private Regex PartOfCollection { get; } = new Regex(@"\[\d+\]");
+        private string GenerateJson(EnvDTE.Expression currentExpression)
+        {
+            if(RunTimeTimer.ElapsedMilliseconds > 10000)
+            {
+                throw new TimeoutException("Timeout while generating JSON.");
+            }
+            Dispatcher.VerifyAccess();
+            if (ExpressionIsDictionary(currentExpression))
+            {
+                var ret = string.Empty;
+                foreach (EnvDTE.Expression dicSubExpression in currentExpression.DataMembers)
+                {
+                    if (PartOfCollection.IsMatch(dicSubExpression.Name))
+                    {
+                        string key = null;
+                        string value = null;
+                        foreach (EnvDTE.Expression dicCollectionExpression in dicSubExpression.DataMembers)
+                        {
+                            if (dicCollectionExpression.Name == "key") 
+                            {
+                                if (ExpressionIsValue(dicCollectionExpression) || ExpressionIsListOrArray(dicCollectionExpression))
+                                {
+                                    key = GenerateJson(dicCollectionExpression);
+                                }
+                                else
+                                {
+                                    key = $"{{{GenerateJson(dicCollectionExpression)}}}";
+                                }
+                               
+                            }
+                            if(dicCollectionExpression.Name == "value")
+                            {
+                                if (ExpressionIsValue(dicCollectionExpression) || ExpressionIsListOrArray(dicCollectionExpression))
+                                {
+                                    value = GenerateJson(dicCollectionExpression);
+                                }
+                                else
+                                {
+                                    value = $"{{{GenerateJson(dicCollectionExpression)}}}";
+                                }                               
+                            }
+                            
+                        }
+                        ret += $"{key}:{value},";
+                    }
+                }
+                ret = ret.TrimEnd(',');
+                return ret;
+            }
+
+
+            if (ExpressionIsValue(currentExpression))
+            {
+                return $"{GetJsonRepresentationofValue(currentExpression)}";
+            }
+            else if (ExpressionIsListOrArray(currentExpression))
+            {
+                var ret = string.Empty;
+                ret += "[";
+
+                foreach (EnvDTE.Expression ex in currentExpression.DataMembers)
+                {
+                    if (PartOfCollection.IsMatch(ex.Name))
+                    {
+                        if (ExpressionIsValue(ex) || ExpressionIsListOrArray(ex))
+                        {
+                            ret += GenerateJson(ex);
+                        }
+                        else
+                        {
+                            ret += $"{{{GenerateJson(ex)}}}";
+                        }
+                        ret += ",";
+                    }                    
+                }
+                ret = ret.TrimEnd(',');
+                ret += $"]";
+                return ret;
+            }
+            else
+            {
+                var ret = string.Empty;
+                foreach (EnvDTE.Expression subExpression in currentExpression.DataMembers)
+                {
+                    if (subExpression.Value == "null")
+                    {
+                        ret += $"\"{subExpression.Name}\":null";
+                    }
+                    else if (ExpressionIsValue(subExpression) || ExpressionIsListOrArray(subExpression))
+                    {
+                        ret += $"\"{subExpression.Name}\":{GenerateJson(subExpression)}";
+                    }
+
+                    else
+                    {
+                        ret += $"\"{subExpression.Name}\":{{{GenerateJson(subExpression)}}}";
+                    }
+                    ret += ",";
+                }
+                ret = ret.TrimEnd(',');
+                return ret;
+            }
+
+        }
+
+        private string GetJsonRepresentationofValue(EnvDTE.Expression exp)
+        {
+            Dispatcher.VerifyAccess();
+            switch (exp.Type.Trim('?'))
+            {
+                case "System.DateTime":
+                    return $"\"{exp.Value.Replace("{", "").Replace("}", "")}\"";
+                case "int":
+                case "string":
+                case "bool":
+                case "double":
+                case "float":
+                case "decimal":
+                case "long":
+                    return exp.Value;
+                case "char":
+                    return $"\"{exp.Value.Substring(exp.Value.IndexOf("'") + 1, 1)}\"";
+                default:
+                    return string.Empty;
+            }
+        }
+
+        private bool ExpressionIsListOrArray(EnvDTE.Expression exp)
+        {
+            Dispatcher.VerifyAccess();
+            if (exp.Type.StartsWith("System.Collections.Generic.List"))
+            {
+                return true;
+            }
+            if (exp.Type.EndsWith("[]"))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool ExpressionIsDictionary(EnvDTE.Expression exp)
+        {
+            Dispatcher.VerifyAccess();
+            if (exp.Type.StartsWith("System.Collections.Generic.Dictionary"))
+            {
+                return true;
+            }          
+            return false;
+        }
+
+        private bool ExpressionIsValue(EnvDTE.Expression exp)
+        {
+            Dispatcher.VerifyAccess();
+            switch (exp.Type.Trim('?'))
+            {
+                case "string":
+                case "System.DateTime":
+                case "int":
+                case "char":
+                case "bool":
+                case "double":
+                case "float":
+                case "decimal":
+                case "long":
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
